3e4ae4ff0a56e4867eee92417a2706fe5b778629
Tsubasa Maeno <tsubasa.maeno@gmail.com>
2021-10-23 08:51:24 +0900

    [update] number string

---
+++ src/number-string.ts
@@ -0,0 +1,13 @@
+export {};
+
+let year: number = 1982;
+// year = false;
+console.log(year);
+
+let age: number = 0x26;
+
+console.log(age);
+
+let name: string = 'Hoge';
+// name = 10;
+console.log(name);
