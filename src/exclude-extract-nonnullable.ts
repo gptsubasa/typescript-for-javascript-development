ca4d7e1496bd0ea8bd772769956d0db19f17aff8
Tsubasa Maeno <tsubasa.maeno@gmail.com>
2021-11-06 11:11:59 +0900

    [update] Exclude と Exract と NunNullable

---
+++ src/exclude-extract-nonnullable.ts
@@ -0,0 +1,16 @@
+import { SourceMap } from "module";
+
+export {};
+
+type MyExclude<T, U> = T extends U ? never : T;
+
+
+type DebugType = () => void;
+type SomeTypes = string | number | DebugType;
+type FunctionType = Exclude<SomeTypes, string | number>;
+type MyFunctionType = MyExclude<SomeTypes, string | number>;
+
+type FunctionTypeByExtract = Extract<SomeTypes, DebugType>;
+
+type NullableTypes = string | number | null | undefined;
+type NonNullableTypes = NonNullable<NullableTypes>;     // string | number
