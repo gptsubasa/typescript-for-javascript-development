3fc3e52990e8cf6841bca4550f74c2a30ee8aa9b
Tsubasa Maeno <tsubasa.maeno@gmail.com>
2021-11-07 10:13:53 +0900

    [update] ReturnType

---
+++ src/return-types.ts
@@ -0,0 +1,11 @@
+export {};
+
+function add(a:number, b:number){
+    return a + b;
+}
+
+console.log(add(1, 2));
+
+// 関数の戻り値の型を取り出して設定
+type ReturnTypeFromAdd = ReturnType<typeof add>;    // number型
+
