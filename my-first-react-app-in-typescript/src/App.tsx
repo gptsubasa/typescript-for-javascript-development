d1e37c1214547befdb08f7566b3d2ade1e0a12c1
Tsubasa Maeno <tsubasa.maeno@gmail.com>
2021-11-10 08:44:49 +0900

    [update] 68. Function Components 初級編

--- my-first-react-app-in-typescript/src/App.tsx
+++ my-first-react-app-in-typescript/src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
 interface AppProps {
-  message: string;
+  message?: string;
+  description?: string;
 }
 
 // const App = (props: any) => {
 //   const { message } = props;  // JavaScriptの分割代入
-const App = ({ message }: AppProps) => {
+
+// 67. Function Components 入門編
+// const App = ({ message }: AppProps) => {
+//   return <div>{message}</div>;
+// };
+
+// 68. Function Components 初級編
+const App: React.FunctionComponent<AppProps> = ({ message }) => {
   return <div>{message}</div>;
+};
+App.defaultProps = {
+  message: 'Hello, defaultProps!',
+  description: 'This is App component.',
 };
 
 export default App;
