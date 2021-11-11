86a4c90af7852a6cfb5960b2874f5976ba310e83
Tsubasa Maeno <tsubasa.maeno@gmail.com>
2021-11-12 08:26:50 +0900

    [update] 70. useRefとuseEffect

---
+++ my-first-react-app-in-typescript/src/70.Counter.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+// const array: Array<number> = [1, 2, 3];
+// const readonlyArray: ReadonlyArray<number> = [1, 2, 3];
+// array[0] = 11;
+// // readonlyArray[0] = 11;   // error
+
+const Counter: React.FC<{}> = () => {
+    const initialValue: any = 0;
+    const [value, setValue] = useState<number>(initialValue);
+
+    const increment = () => {
+        setValue((prevState) => prevState + 1);
+    };
+
+    const decrement = () => {
+        setValue((prevState) => prevState - 1);
+    };
+
+    const renderTimes = useRef<number>(0);
+    useEffect(() => {
+        renderTimes.current += 1;
+        console.log(`renderTimes: ${renderTimes.current}`);
+    });
+
+    return (
+        <div>
+            <div>value: {value}</div>
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+            <div>This component was re-rendered {renderTimes.current} times!</div>
+        </div>
+    );
+}
+
+export default Counter;
--- my-first-react-app-in-typescript/src/App.tsx
+++ my-first-react-app-in-typescript/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Counter from './Counter';
+import Counter from './70.Counter';
 
 interface AppProps {
   message?: string;
